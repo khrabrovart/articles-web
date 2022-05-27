f38fc2fe8aa2f6fb6b80e0fab7a828b180645f24
Arthur Khrabrov <khrabrovart@gmail.com>
2022-05-27 11:44:59 +0400

    Fix paths

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,26 +1,26 @@
 module "s3" {
-  source            = "./modules/s3"
+  source            = "modules/s3"
   bucket_name       = "articles-frontend"
-  content_directory = "./dist"
+  content_directory = "dist"
   common_tags       = var.common_tags
 }
 
 module "cloudfront" {
-  source           = "./modules/cloudfront"
+  source           = "modules/cloudfront"
   bucket           = module.s3.bucket
   website_endpoint = module.s3.website_endpoint
   common_tags      = var.common_tags
 }
 
 /*module "lambda_comments_get" {
-  source           = "./modules/lambda-function"
+  source           = "modules/lambda-function"
   function_name    = "Articles_Comments_Get"
   function_handler = "ArticlesWeb.Functions.Comments.Get::ArticlesWeb.Functions.Comments.Get.Function::Handler"
   common_tags      = var.common_tags
 }
 
 module "lambda_comments_create" {
-  source           = "./modules/lambda-function"
+  source           = "modules/lambda-function"
   function_name    = "Articles_Comments_Create"
   function_handler = "ArticlesWeb.Functions.Comments.Create::ArticlesWeb.Functions.Comments.Create.Function::Handler"
   common_tags      = var.common_tags
