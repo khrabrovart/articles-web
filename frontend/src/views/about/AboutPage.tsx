660d3a9e4128364009c54f6461a962b13f76d18d
Arthur Khrabrov <khrabrovart@gmail.com>
2022-05-27 23:46:51 +0400

    Add new version

--- frontend/src/views/about/AboutPage.tsx
+++ frontend/src/views/about/AboutPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Page from "../Page";
 
 const AboutPage = () => {
-  return <Page>This is my about page! New version!</Page>;
+  return <Page>This is my about page! New version V2</Page>;
 };
 
 export default AboutPage;
