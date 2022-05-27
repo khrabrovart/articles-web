15692d7c6f390fb83947ed7948fb6cf579e63665
Arthur Khrabrov <khrabrovart@gmail.com>
2022-05-27 22:44:20 +0400

    Add post-deploy job

--- frontend/src/views/about/AboutPage.tsx
+++ frontend/src/views/about/AboutPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Page from "../Page";
 
 const AboutPage = () => {
-  return <Page>This is my about page!</Page>;
+  return <Page>This is my about page! New version!</Page>;
 };
 
 export default AboutPage;
