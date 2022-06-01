43330fdd6e3c122f1bda451156d37510a823657d
Arthur Khrabrov <khrabrovart@gmail.com>
2022-06-01 22:04:21 +0400

    Optimize functions list

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,18 +1,14 @@
 locals {
   functions = {
-    articles_comments_get = {
-      name        = "Articles_Comments_Get"
-      source_dir  = "./artifacts/backend/ArticlesWeb.Functions.Comments.Get"
-      handler     = "ArticlesWeb.Functions.Comments.Get::ArticlesWeb.Functions.Comments.Get.Function::Handler"
-      http_method = "GET"
-      http_route  = "/comments"
+    Articles_Comments_Get = {
+      project_name = "ArticlesWeb.Functions.Comments.Get"
+      http_method  = "GET"
+      http_route   = "/comments"
     },
-    articles_comments_create = {
-      name        = "Articles_Comments_Create"
-      source_dir  = "./artifacts/backend/ArticlesWeb.Functions.Comments.Create"
-      handler     = "ArticlesWeb.Functions.Comments.Create::ArticlesWeb.Functions.Comments.Create.Function::Handler"
-      http_method = "POST"
-      http_route  = "/comments"
+    Articles_Comments_Create = {
+      project_name = "ArticlesWeb.Functions.Comments.Create"
+      http_method  = "POST"
+      http_route   = "/comments"
     }
   }
 }
@@ -22,9 +18,9 @@ module "lambda_functions" {
 
   for_each = local.functions
 
-  function_name       = each.value.name
-  function_source_dir = each.value.source_dir
-  function_handler    = each.value.handler
+  function_name       = each.key
+  function_source_dir = "./artifacts/backend/${each.value.project_name}"
+  function_handler    = "${each.value.project_name}::${each.value.project_name}.Function::Handler"
   common_tags         = var.common_tags
 }
 
@@ -35,7 +31,7 @@ module "api_gateway" {
   common_tags = var.common_tags
 }
 
-module "api_gateway_integration" {
+/* module "api_gateway_integration" {
   source = "./modules/api-gateway-integration"
 
   for_each = local.functions
@@ -43,9 +39,9 @@ module "api_gateway_integration" {
   api_id            = module.api_gateway.id
   api_execution_arn = module.api_gateway.execution_arn
 
-  function_name       = each.value.name
+  function_name       = each.key
   function_invoke_arn = module.lambda_functions[each.key].invoke_arn
 
   integration_method = each.value.http_method
   route_key          = "${each.value.http_method} ${each.value.http_route}"
-}
+} */
