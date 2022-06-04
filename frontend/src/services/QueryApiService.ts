28b3587eeebe6fd375b2ce40ebc6a7e58053760f
Arthur Khrabrov <khrabrovart@gmail.com>
2022-06-04 23:00:18 +0400

    Rewrite api origin

--- frontend/src/services/QueryApiService.ts
+++ frontend/src/services/QueryApiService.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const getBaseUrl = (): string => {
-  if (!process.env.API_BASE_URI || !process.env.API_PROTOCOL) {
-    throw new Error("API URI is not defined");
-  }
-
-  return `${process.env.API_PROTOCOL}://${process.env.API_BASE_URI}`;
-};
+const getBaseUrl = (): string =>
+  process.env.API_ORIGIN ? process.env.API_ORIGIN : location.origin;
 
 export const httpGet = <T>(route: string): Promise<T> =>
   axios.get(`${getBaseUrl()}/api/${route}`);
