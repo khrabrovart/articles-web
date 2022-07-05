c495074ac196f4f4a6e778d47e7b1aa0f99518cc
Arthur Khrabrov <khrabrovart@gmail.com>
2022-07-06 00:19:27 +0400

    Update functions

--- backend/ArticlesWeb.Functions.Comments.Create/Function.cs
+++ backend/ArticlesWeb.Functions.Comments.Create/Function.cs
@@ -1,8 +1,10 @@
 ﻿using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using ArticlesWeb.Functions.Shared;
 
 [assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
@@ -10,22 +12,17 @@ namespace ArticlesWeb.Functions.Comments.Create;
 
 public class Function
 {
-    public string[] Handler(string input, ILambdaContext ctx)
+    public async Task<APIGatewayProxyResponse> Handler(string input, ILambdaContext ctx)
     {
         LambdaLogger.Log("Some message in logs");
 
         var c = new AmazonDynamoDBClient(RegionEndpoint.USEast1);
 
-        c.PutItemAsync("comments", new Dictionary<string, AttributeValue>
+        await c.PutItemAsync("comments", new Dictionary<string, AttributeValue>
         {
             {"key1", new AttributeValue("val1")}
         });
 
-        return new[]
-        {
-            "Comment 1",
-            "Comment 2",
-            input
-        };
+        return APIGatewayHelpers.BuildResponse("successful response 111");
     }
 }
