70d26af7611a8bb5b234696019cc00237171fbb5
Arthur Khrabrov <khrabrovart@gmail.com>
2022-07-11 23:36:21 +0400

    Rollback changes

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,7 +1,7 @@
 module "s3" {
   source = "./modules/s3"
 
-  bucket_name       = "articles-frontend-${var.s3_name_postfix}"
+  bucket_name       = "articles-frontend-${var.region}"
   content_directory = "./artifacts/frontend"
   common_tags       = var.common_tags
 }
